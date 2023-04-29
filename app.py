78ffa373dc96ebeddca38643e57928a8d2b324cc
lilyichise <95495611+lilyichise@users.noreply.github.com>
2023-04-29 15:29:25 -0400

    changes

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
-from google.auth.transport import requests
-from google.oauth2 import id_token
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 from flask import Flask, render_template, redirect, url_for, session, request
 from database_utils import StudyGroupDatabase
 
 app = Flask(__name__)
 app.secret_key = "1234python"
+
+# set path for database
 DATABASE = 'study_groups.db'
 
 
 def get_db():
+    """
+    function to get database
+    """
     if 'db' not in g:
         g.db = StudyGroupDatabase(DATABASE)
     return g.db
@@ -24,38 +27,95 @@ def close_connection(exception):
 
 
 @app.route('/')
-def index():
+def homepage():
     db = get_db()
     users = db.get_users()
-    courses = db.search_courses('math')
+    courses = db.search_courses('')
     return render_template('homepage.html', users=users, courses=courses)
 
 
 # Route for the login page
 @app.route("/login")
 def login():
+    # check if the user is already logged in
+    if 'user_id' in session:
+        return redirect(url_for('homepage'))
+
+    # handle the login form submission
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # check if the user exists in the database
+        db = get_db()
+        user_id = db.check_user(email, password)
+
+        if user_id:
+            # set the user_id session variable and redirect to the homepage
+            user = User(user_id)
+            login_user(user)
+            return redirect(url_for('homepage'))
+        else:
+            # if the user does not exist, display an error message
+            error = "Invalid email or password. Please try again."
+            return render_template('login.html', error=error)
+
+    # display the login form
     return render_template('login.html')
 
+# create the route for the signup page
 
-# Route for signup page
 
-
-@app.route("/signup")
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    # check if the user is already logged in
+    if 'user_id' in session:
+        return redirect(url_for('homepage'))
+
+    # handle the signup form submission
     if request.method == 'POST':
-        # Do something with the form data
-        return redirect(url_for('index'))
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        # add the user to the database and set the user_id session variable
+        db = get_db()
+        user_id = db.add_user(name, email, password)
+        user = User(user_id)
+        login_user(user)
+
+        # redirect to the homepage
+        return redirect(url_for('homepage'))
+
+    # display the signup form
     return render_template('signup.html')
 
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-
+    # get the user's information from the database
     db = get_db()
-    if request.method == 'POST':
+    # Handle the course code search and add functionality
+    if request.method == 'POST' and 'search_course_code' in request.form:
+        # Get the course code entered by the user
+        course_code = request.form['search_course_code']
+
+        # Check if the course code already exists in the database
+        if db.get_course_by_code(course_code) is not None:
+            # If the course code already exists, set it as the selected option
+            selected_course = course_code
+        else:
+            # If the course code does not exist, add it to the database
+            db.add_course(course_code)
+            selected_course = course_code
+
+        # Get the user's data
+        user_data = db.get_user_data(session['user_id'])
+        return render_template('profile.html', user_data=user_data, selected_course=selected_course)
+
+    # Handle the form submission
+    elif request.method == 'POST':
         name = request.form['name']
         course_code = request.form['course_code']
         meet_days = request.form['meet_days']
@@ -63,41 +123,18 @@ def profile():
         group_size = request.form['group_size']
         work_style = request.form['work_style']
         goal = request.form['goal']
-        # insert the form data into the database
+
+        # Insert the form data into the database
         db.insert_user_data(session['user_id'], name, course_code,
                             meet_days, meet_times, group_size, work_style, goal)
 
-    user_data = db.get_user_data(session['user_id'])
-    return render_template('profile.html', user_data=user_data)
+        # Redirect to the profile page
+        return redirect(url_for('profile'))
 
-
-google = oauth.remote_app(
-    'google',
-    consumer_key='375669104663-n3ocd5qaldera612mnt19cg27i4ar0sq.apps.googleusercontent.com',
-    consumer_secret='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',
-    request_token_params={
-        'scope': 'email'
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-)
-
-
-@app.route('/login')
-def login():
-    return google.authorize(callback=url_for('callback', _external=True))
-
-
-@app.route('/callback')
-def callback():
-    access_token = google.authorized_response()['access_token']
-    session['access_token'] = access_token
-    user_info = requests.get(
-        'https://www.googleapis.com/oauth2/v1/userinfo', params={'access_token': access_token})
-    return "Hello, {}!".format(user_info.json()['email'])
+    # If the request method is GET, display the profile page with the user's data
+    else:
+        user_data = db.get_user_data(session['user_id'])
+        return render_template('profile.html', user_data=user_data)
 
 
 if __name__ == '__main__':
